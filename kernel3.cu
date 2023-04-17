9bf2cd00b1650b192a600e053a34a6dc405c7cca
seanaddington <sean.m.addington@gmail.com>
2023-04-16 22:33:09 -0400

    should resolve 1 issue

--- kernel3.cu
+++ kernel3.cu
@@ -3,8 +3,14 @@ __global__ void kernel3(double *tab, double *colk, int k, int r, int n, int m)
 int i = blockDim.x * blockIdx.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
     __shared__ double w[16];
-    if ( (j + m*i) == (r + 2*i) ) {
-       return;
+    if (j == r) {
+        return;
+    }
+    if (i > m+1) {
+        return;
+    }  
+    if (j > m+1) {
+        return;
     }    
     if (threadIdx.y == 0 && threadIdx.x < 16)
     {
